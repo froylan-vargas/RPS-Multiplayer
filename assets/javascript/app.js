265218a1142b3f13c0eaef20da3bd0cd6d70846c
froylan.vargas <froylan.vargas.gomez@gmail.com>
2018-09-28 18:02:41 -0500

    Game logic, sections css, chat ui and chat css

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,19 +1,24 @@
 
 $(document).ready(function () {
 
+    var isHostChoose = false;
+
     function start(){
         gettingStoredPlayerName();
     }
 
     function gettingStoredPlayerName(){
         var playerName = localStorage.getItem('playerName');
-        if (playerName.length){
+        if (playerName){
             $('#userLabel').text(`Hello ${playerName}`);
             $('#playerInfo').hide();
             $('#rpsArea').show();
+        } else {
+            $('#playerInfo').show();
         }
     }
     
+    //Handlers
     function playHandler() {
         var playerName = $('#nameInput').val().trim();
         if(playerName.length){
@@ -26,6 +31,35 @@ $(document).ready(function () {
         }
     }
 
+    function userChoiceHandler() {
+        var selectedValue = $(this).attr('data-value');
+        if(!isHostChoose){
+            isHostChoose = true;
+            $('#selectionsDiv').show();
+            $('#hostChoice')
+                .attr('src',`./assets/images/${selectedValue}.png`)
+                .attr('data-value', selectedValue);
+        } else {
+            $('#oponentChoice').attr('src',`./assets/images/${selectedValue}.png`);
+            const hostChoice = $('#hostChoice').attr('data-value');
+            ValidateWinner(hostChoice,selectedValue);
+        }
+    }
+
+    //Helpers
+    function ValidateWinner(hostChoice, enemyChoice){
+        if((hostChoice === 'rock' && enemyChoice === 'scissors')
+        || (hostChoice === 'paper' && enemyChoice === 'rock')
+        || (hostChoice === 'scissors' && enemyChoice === 'paper')) {
+            $('#gameResult').text('You win');
+        } else if (hostChoice === enemyChoice) {
+            $('#gameResult').text('Tide Game');
+        } else {
+            $('#gameResult').text('You loose');
+        }
+    }
+
     $('#playButton').on('click', playHandler);
+    $('.options').on('click', userChoiceHandler)
     start();
 });
