c8b9743cc5f7e0c7529114490914736d90c93a09
froylan.vargas <froylan.vargas.gomez@gmail.com>
2018-09-28 15:12:38 -0500

    Adding player name and starting images

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,31 @@
 
 $(document).ready(function () {
-    alert("Im in Rock Paper Scissors");
+
+    function start(){
+        gettingStoredPlayerName();
+    }
+
+    function gettingStoredPlayerName(){
+        var playerName = localStorage.getItem('playerName');
+        if (playerName.length){
+            $('#userLabel').text(`Hello ${playerName}`);
+            $('#playerInfo').hide();
+            $('#rpsArea').show();
+        }
+    }
+    
+    function playHandler() {
+        var playerName = $('#nameInput').val().trim();
+        if(playerName.length){
+            localStorage.setItem('playerName', playerName);
+            $('#userLabel').text(`Hello ${playerName}`);
+            $('#playerInfo').hide();
+            $('#rpsArea').show();
+        } else {
+            alert("You must enter a player name");
+        }
+    }
+
+    $('#playButton').on('click', playHandler);
+    start();
 });
