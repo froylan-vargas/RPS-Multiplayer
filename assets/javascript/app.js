f4ecd67bf420061c608920eb10a718c9287ac9ce
froylan.vargas <froylan.vargas.gomez@gmail.com>
2018-09-28 21:47:31 -0500

    Finishing css and a simple refactor

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,66 +3,95 @@ $(document).ready(function () {
 
     var isHostChoose = false;
 
-    function start(){
+    function start() {
         gettingStoredPlayerName();
     }
 
-    function gettingStoredPlayerName(){
+    //Display
+
+    function gettingStoredPlayerName() {
         var playerName = localStorage.getItem('playerName');
-        if (playerName){
-            $('#userLabel').text(`Hello ${playerName}`);
-            $("#gameContainer").show();
-            $('#playerInfo').hide();
-            $('#rpsArea').show();
-            $('#chatArea').show();
+        if (playerName) {
+            displayAfterPlayerName(playerName);
         } else {
-            $('#playerInfo').show();
+            show('playerInfo');
         }
     }
-    
+
+    function displayAfterPlayerName(playerName) {
+        changeText('userLabel',`Hello ${playerName}`);
+        show("gameContainer");
+        show('rpsArea');
+        show('chatArea');
+        hide('playerInfo');
+    }
+
     //Handlers
     function playHandler() {
         var playerName = $('#nameInput').val().trim();
-        if(playerName.length){
+        if (playerName.length) {
             localStorage.setItem('playerName', playerName);
-            $('#userLabel').text(`Hello${playerName}`);
-            $("#gameContainer").show();
-            $('#playerInfo').hide();
-            $('#rpsArea').show();
-            $('#chatArea').show();
+            displayAfterPlayerName(playerName);
         } else {
             alert("You must enter a player name");
         }
     }
 
     function userChoiceHandler() {
-        var selectedValue = $(this).attr('data-value');
-        if(!isHostChoose){
+        var selectedValue = getAttribute(this, 'data-value');
+        var hostElement = '#hostChoice';
+        if (!isHostChoose) {
             isHostChoose = true;
-            $('#selectionsDiv').show();
-            $('#hostChoice')
-                .attr('src',`./assets/images/${selectedValue}.png`)
-                .attr('data-value', selectedValue);
+            show('selectionsDiv');
+            setAttribute(hostElement, 'src', `./assets/images/${selectedValue}.png`);
+            setAttribute(hostElement, 'data-value', selectedValue);
         } else {
-            $('#oponentChoice').attr('src',`./assets/images/${selectedValue}.png`);
-            const hostChoice = $('#hostChoice').attr('data-value');
-            ValidateWinner(hostChoice,selectedValue);
+            const hostChoice = getAttribute(hostElement, 'data-value');
+            setAttribute('#oponentChoice', 'src', `./assets/images/${selectedValue}.png`);
+            ValidateWinner(hostChoice, selectedValue);
         }
     }
 
     //Helpers
-    function ValidateWinner(hostChoice, enemyChoice){
-        if((hostChoice === 'rock' && enemyChoice === 'scissors')
-        || (hostChoice === 'paper' && enemyChoice === 'rock')
-        || (hostChoice === 'scissors' && enemyChoice === 'paper')) {
-            $('#gameResult').text('You win');
-        } else if (hostChoice === enemyChoice) {
-            $('#gameResult').text('Tide Game');
-        } else {
-            $('#gameResult').text('You loose');
-        }
+    function setAttribute(element, attr, value) {
+        $(element).attr(attr, value);
     }
 
+    function changeText(element,value){
+        $(`#${element}`).text(value);
+    }
+
+    function show(element) {
+        $(`#${element}`).show();
+    }
+
+    function hide(element) {
+        $(`#${element}`).hide();
+    }
+
+    function getAttribute(element, attr) {
+        return $(element).attr(attr);
+    }
+
+    function ValidateWinner(hostChoice, enemyChoice) {
+        var result = '';
+        if ((hostChoice === 'rock' && enemyChoice === 'scissors')
+            || (hostChoice === 'paper' && enemyChoice === 'rock')
+            || (hostChoice === 'scissors' && enemyChoice === 'paper')) {
+            result = 'You win';
+        } else if (hostChoice === enemyChoice) {
+            result = 'Tide Game';
+        } else {
+            result = 'You lose!'
+        }
+        printGameResult(result);
+    }
+
+    function printGameResult(message) {
+        $('#gameResult').text(message);
+    }
+
+    //Bindings
     $('#playButton').on('click', playHandler);
     $('.options').on('click', userChoiceHandler)
     start();
