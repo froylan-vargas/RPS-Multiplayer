9d41549d1a55fc02f8cacb7a5694cbede4b4b223
froylan.vargas <froylan.vargas.gomez@gmail.com>
2018-09-29 02:57:10 -0500

    Starting firebase available players logic

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,71 @@
-
 $(document).ready(function () {
 
+    var config = {
+        apiKey: "AIzaSyAgZHBWeEp1QXA8tqDh-kq43cnvFj4Ih1k",
+        authDomain: "rps-multiplayer-c0ef6.firebaseapp.com",
+        databaseURL: "https://rps-multiplayer-c0ef6.firebaseio.com",
+        projectId: "rps-multiplayer-c0ef6",
+        storageBucket: "rps-multiplayer-c0ef6.appspot.com",
+        messagingSenderId: "289792024556"
+    };
+
+    firebase.initializeApp(config);
+    var database = firebase.database();
+
+    var connectionsRef = database.ref("/connections");
+    var connectedRef = database.ref(".info/connected");
+    var userKey;
     var isHostChoose = false;
     var playerName = '';
 
+    connectedRef.on("value", function (snap) {
+        if (snap.val()) {
+            var con = connectionsRef.push(true);
+            userKey = con.key;
+            con.onDisconnect().remove();
+        }
+    });
+
+    database.ref(`/connections`).on("child_changed", function(snap){
+        console.log('added');
+        var users = $("#users");
+        if(snap.val().playerName){
+            if(snap.val().playerName !== playerName){
+                var newUser = $('<p>').text(snap.val().playerName);
+                users.append(newUser);
+            }
+        }
+    });
+
+    database.ref(`/connections`).on("child_removed", function(snap){
+        console.log(snap.val());
+        
+    });
+
+    database.ref('/connections').once("value", function(snap){
+        console.log('once');
+        var users = $("#users");
+        snap.forEach(connection => {
+            if(connection.val().playerName){
+                if(connection.val().playerName !== playerName){
+                    var newUser = $('<p>').text(connection.val().playerName);
+                    users.append(newUser);
+                }
+            }
+        });
+    })
+
+    
+
+    function playTestHandler() {
+        playerName = $('#nameInput').val().trim();
+        database.ref(`/connections/${userKey}`).set({
+            playerName,
+            available: true
+          });
+    }
+
+    
     function start() {
         gettingStoredPlayerName();
     }
@@ -11,16 +73,17 @@ $(document).ready(function () {
     //Display
 
     function gettingStoredPlayerName() {
-        playerName = localStorage.getItem('playerName');
+        /*playerName = localStorage.getItem('playerName');
         if (playerName) {
             displayAfterPlayerName(playerName);
         } else {
             show('playerInfo');
-        }
+        }*/
+        show('playerInfo');
     }
 
     function displayAfterPlayerName(playerName) {
-        changeText('userLabel',`Hello ${playerName}`);
+        changeText('userLabel', `Hello ${playerName}`);
         show("gameContainer");
         show('rpsArea');
         show('chatArea');
@@ -28,6 +91,8 @@ $(document).ready(function () {
     }
 
     //Handlers
+    
+
     function playHandler() {
         playerName = $('#nameInput').val().trim();
         if (playerName.length) {
@@ -53,21 +118,21 @@ $(document).ready(function () {
         }
     }
 
-    function sendMessageHandler(){
+    function sendMessageHandler() {
         var message = getInputValue('messageInput');
         var chat = $('#chat');
-        if (message){
-          var newMessage = createMessageElement(message);
-          cleanInputValue('messageInput');
-          chat.append(newMessage);
-        } 
+        if (message) {
+            var newMessage = createMessageElement(message);
+            cleanInputValue('messageInput');
+            chat.append(newMessage);
+        }
     }
 
     //Helpers
-    function createMessageElement(message, isHost = true){
+    function createMessageElement(message, isHost = true) {
         var newMessage = $('<p>');
         newMessage.text(`${playerName}: ${message}`);
-        if (isHost){
+        if (isHost) {
             newMessage.addClass('hostMessage');
         } else {
             newMessage.addClass('oponentMessage');
@@ -75,7 +140,7 @@ $(document).ready(function () {
         return newMessage;
     }
 
-    function cleanInputValue(element){
+    function cleanInputValue(element) {
         $(`#${element}`).val('');
     }
 
@@ -83,11 +148,11 @@ $(document).ready(function () {
         $(element).attr(attr, value);
     }
 
-    function getInputValue(element){
+    function getInputValue(element) {
         return $(`#${element}`).val();
     }
 
-    function changeText(element,value){
+    function changeText(element, value) {
         $(`#${element}`).text(value);
     }
 
@@ -122,10 +187,11 @@ $(document).ready(function () {
     }
 
     //Bindings
-    $('#playButton').on('click', playHandler);
+    //$('#playButton').on('click', playHandler);
+    $('#playButton').on('click', playTestHandler);
     $('#sendMessageButton').on('click', sendMessageHandler);
     $('.options').on('click', userChoiceHandler);
-    
+
     //GameEvents
     start();
 });
